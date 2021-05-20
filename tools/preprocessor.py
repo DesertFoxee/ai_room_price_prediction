7e4a2b48a65f5543fb08338f33a3dcd6fdcedaa1
DesertFoxee <DesertFoxexe@gmail.com>
2021-05-21 00:40:15 +0700

    ++ Them truong quan

--- tools/preprocessor.py
+++ tools/preprocessor.py
@@ -21,6 +21,27 @@ folder_out_spider= "data_pre/data_daily/"
 path_data_raw = 'data_train/data_phongtro123_data.csv'
 path_data_train = 'roomdata.csv'
 
+path_data_raw2 = 'data_train/data_phongtro123_databk.csv'
+path_data_train2 = 'roomdata2.csv'
+
+district_convert ={
+    'Thanh Xuân'  :"ThanhXuan" ,
+    'Hoàng Mai'   :"HoangMai"  ,
+    'Bắc Từ Liêm' :"BacTuLiem" ,
+    'Cầu Giấy'    :"CauGiay"   ,
+    'Hà Đông'     :"HaDong"    ,
+    'Đống Đa'     :"DongDa"    ,
+    'Nam Từ Liêm' :"NamTuLiem" ,
+    'Hoàn Kiếm'   :"HoanKiem"  ,
+    'Hai Bà Trưng':"HaiBaTrung",
+    'Tây Hồ'      :"TayHo"     ,
+    'Ba Đình'     :"BaDinh"    ,
+    'Gia Lâm'     :"GiaLam"    ,
+    'Long Biên'   :"LongBien"  ,
+    'Thanh Trì'   :"ThanhTri"  ,
+    'Hoài Đức'    :"HoaiDuc"
+}
+
 
 def drop_row(data_frame, start_index , end_index):
     data_frame.drop(data_frame.index[0: start_index], inplace=True)
@@ -198,13 +219,27 @@ def convert_rawdata_to_traindata(path_rawdata_in , path_traindata_out):
     df.to_csv(path_traindata_out, index=False, header=True)
 
 
+# Xử lý các cột trong dữ liệu thô sang file housepricedata_1.csv
+def convert_rawdata_to_traindata2(path_rawdata_in , path_traindata_out):
+    df = pd.read_csv(path_rawdata_in ,encoding='utf8')
+    df.drop(['stt', 'chitiet', 'diachi','tiennghi','thuantien','duong', 'phuong'], axis=1, inplace=True)
+    df["thoigian"] = pd.to_datetime(df["thoigian"])
+    df.insert(1, 'thang', df["thoigian"].dt.month)
+    df.insert(0, 'nam', df["thoigian"].dt.year)
+    df = df.drop(['thoigian'], axis=1)
+
+    df['quan'].replace(district_convert, inplace=True)
+    df.to_csv(path_traindata_out, index=False, header=True)
+
+
 def main():
     # load csv
     # pre_detail_phongtro123(path_data_raw_01)
     # preprocessing_data_phongtro123("data_raw/data_phongtro123_03052021.csv")
 
 
-    convert_rawdata_to_traindata(path_data_raw, path_data_train)
+    # convert_rawdata_to_traindata(path_data_raw, path_data_train)
+    convert_rawdata_to_traindata2(path_data_raw2, path_data_train2)
     # convert_data_row(path_data_raw , path_data_out)
     return
 
